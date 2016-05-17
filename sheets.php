fcc346c133349f1e76965e76167bc022a50a2136
DarrellJChristensen <68senior19@gmail.com>
2016-05-17 16:03:30 -0600

    added h2 message

--- sheets.php
+++ sheets.php
@@ -45,18 +45,21 @@
             <div class="col-md-4">
                 <img src="lib/images/tacobox.jpg" width="210px" height="210px">
                 <h2>Taco Box</h2>
-                <p></p>
-            </div>
-
-            <div class="col-md-4">
-                <img src="lib/images/tequila.jpg" width="210px" height="210px">
-                <h2>Tequila</h2>
+                <p>777 W. Broadway   Farmimgton,N.M.</p>
             </div>
 
             <div class="col-md-4">
                 <img src="lib/images/teriyakibeefbowl.jpg" width="210px" height="210px">
                 <h2>Teriyaki Chicken & Beef Bowl</h2>
+                <p>3501 E. Main Ste H-Middlefork Sq    Frmt</p>
             </div>
+
+            <div class="col-md-4">
+                <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
+                <h2>Teriyaki Chicken Combo</h2>
+                <p>1808 E. Main   Farmington,N.M.</p>
+            </div>
+
         </div><!--row-->
     </div><!--container-->
 
@@ -66,22 +69,52 @@
 
         <div class="row">
             <div class="col-md-4">
-                <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
-                <h2>Teriyaki Chicken Combo</h2>
-            </div>
+            <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
+            <h2>Teriyaki Chicken Combo</h2>
+            <p>Hwy 64  Kirtland, N.M.</p>
+        </div>
 
+        <div class="col-md-4">
+            <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
+            <h2>Teriyaki Chicken Combo</h2>
+            <p>400 W.Broadway   Bloomfield, N.M.</p>
+        </div>
+
+        <div class="col-md-4">
+            <img src="lib/images/wendylogo.png" width="210px" height="210px">
+            <h2>Wendy's Old Fashion Hamburgers</h2>
+            <p>1820 E. Main   Frmt</p>
+        </div>
+    </div><!--row-->
+
+    </div><!--container-->
+
+
+    <div class="container">
+
+        <div class="row">
             <div class="col-md-4">
-                <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
-                <h2>Teriyaki Chicken Combo</h2>
+                <img src="lib/images/wendylogo.png" width="210px" height="210px">
+                <h2>Wendy's Old Fashion Hamburgers</h2>
+                <p>4903 A  E. Main   Frmt</p>
             </div>
 
             <div class="col-md-4">
                 <img src="lib/images/wendylogo.png" width="210px" height="210px">
                 <h2>Wendy's Old Fashion Hamburgers</h2>
+                <p>2601 W. Main     Frmt</p>
             </div>
+
+            <div class="col-md-4">
+                <img src="lib/images/wendylogo.png" width="210px" height="210px">
+                <h2>Wendy's Old Fashion Hamburgers</h2>
+                <p>1305 Aztec Blvd   Aztec</p>
+            </div>
+
         </div><!--row-->
 
     </div><!--container-->
+
 
 
 
