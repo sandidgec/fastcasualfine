472c639c3525d031f47ec3c5d2474222c500161e
DarrellJChristensen <68senior19@gmail.com>
2016-05-17 15:37:38 -0600

    added h2 message

--- sheets.php
+++ sheets.php
@@ -5,20 +5,100 @@
 
 <head>
 
-    <div class="jumbotron">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6">
-                    <h1>Fast Food</h1>
-                    <p></p>
-                </div>
-            </div>
-        </div>
-    </div>
 
 </head>
 
-<body>
-<h1>Hello Dev Shop Team!</h1>
-</body>
+    <body>
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Fast Food</h1>
+        </div>
+    </div>
+
+        <div class="container">
+
+            <div class="row">
+            <div class="col-md-4">
+                <img src="lib/images/taco.jpg" width="210px" height="210px">
+                <h2>Taco Bell</h2>
+                <p>2226 E. Main   Farmington, N.M.</p>
+            </div>
+
+            <div class="col-md-4">
+                <img src="lib/images/taco.jpg" width="210px" height="210px">
+                <h2>Taco Bell</h2>
+                <p>4601 E. Main   Farmington, N.M.</p>
+            </div>
+
+            <div class="col-md-4">
+                <img src="lib/images/taco.jpg" width="210px" height="210px">
+                <h2>Taco Bell</h2>
+                <p>2100 W. Main   Farmington, N.M.</p>
+            </div>
+        </div><!--row-->
+    </div><!--container-->
+
+    <br><br><br>
+
+        <div class="row">
+            <div class="col-md-4">
+                <img src="lib/images/tacobox.jpg" width="210px" height="210px">
+                <h2>Taco Box</h2>
+                <p></p>
+            </div>
+
+            <div class="col-md-4">
+                <img src="lib/images/tequila.jpg" width="210px" height="210px">
+                <h2>Tequila</h2>
+            </div>
+
+            <div class="col-md-4">
+                <img src="lib/images/teriyakibeefbowl.jpg" width="210px" height="210px">
+                <h2>Teriyaki Chicken & Beef Bowl</h2>
+            </div>
+        </div><!--row-->
+    </div><!--container-->
+
+    <br><br><br>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-4">
+                <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
+                <h2>Teriyaki Chicken Combo</h2>
+            </div>
+
+            <div class="col-md-4">
+                <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
+                <h2>Teriyaki Chicken Combo</h2>
+            </div>
+
+            <div class="col-md-4">
+                <img src="lib/images/wendylogo.png" width="210px" height="210px">
+                <h2>Wendy's Old Fashion Hamburgers</h2>
+            </div>
+        </div><!--row-->
+
+    </div><!--container-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </body>
 </html>
