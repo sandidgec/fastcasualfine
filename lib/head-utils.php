7c29e73d06844ff6bf9f00ac27b88e5a7f34736e
Jeremiah Johnson <jeremiahsir@gmail.com>
2016-05-17 09:29:27 -0600

    added some stuff in head-utils.php

---
+++ lib/head-utils.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<title>head-utils</title>
+<!-- Bootstrap - Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+      integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+      crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="lib/css/style.css">
