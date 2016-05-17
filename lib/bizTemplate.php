4cf70b5a2a59329b06ed480513918e477a497aef
Justinmatece <justin_hildreth16@outlook.com>
2016-05-17 13:26:03 -0600

    test git connection

---
+++ lib/bizTemplate.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <?php require_once("head-utils.php"); ?>
+</head>
+
+<body>
+<div class="container">
+    <div class="jumbotron">
+        <div>
+            <h1><span class="glyphicon glyphicon-cutlery"></span>Si Senor!<span class="glyphicon glyphicon-cutlery"></span></h1>
+            <p>Come check out what everybody is talking about!</p>
+        </div>
+        <p> Easy an realiable </p>
+    </div>
+</div>
+
+<h1>Hello Dev Shop Team!</h1>
+</body>
+</html>
