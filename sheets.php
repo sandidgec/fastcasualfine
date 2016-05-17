1d6db2ae557deb93a3549be80b141991ea425055
DarrellJChristensen <68senior19@gmail.com>
2016-05-17 11:31:19 -0600

    added h2 message

---
+++ sheets.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<?php require_once("lib/head-utils.php"); ?>
+
+<head>
+
+    <div class="jumbotron">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Fast Food</h1>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</head>
+
+<body>
+<h1>Hello Dev Shop Team!</h1>
+</body>
+</html>
