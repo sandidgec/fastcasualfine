f230d8f5554bd5ccd1b0009ad3254c7cc7801d90
DarrellJChristensen <68senior19@gmail.com>
2016-05-18 09:08:48 -0600

    added h2 message

--- sheets.php
+++ sheets.php
@@ -16,101 +16,207 @@
         </div>
     </div>
 
-        <div class="container">
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/arbys.jpg" width="210px" height="210px">
+                <h2>Arby's</h2>
+                <p>1825 E. Main   Farmington, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/blakes.jpg" width="210px" height="210px">
+                <h2>Blakes Lotaburger</h2>
+                <p>1611 W. Main   Farmington, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/blakes.jpg" width="210px" height="210px">
+                <h2>Blakes Lotaburger</h2>
+                <p>420 E. Main   Farmington, N.M.</p>
+            </div>
+
+        </div><!--row-->
+
+        <br><br><br>
 
             <div class="row">
-            <div class="col-md-4">
-                <img src="lib/images/taco.jpg" width="210px" height="210px">
-                <h2>Taco Bell</h2>
-                <p>2226 E. Main   Farmington, N.M.</p>
-            </div>
+                <div class="col-md-4 text-center">
+                    <img src="lib/images/blakes.png" width="210px" height="210px">
+                    <h2>Blakes Lotaburger</h2>
+                    <p> 2220 E. 20th   Farmington, N.M.</p>
+                </div>
 
-            <div class="col-md-4">
-                <img src="lib/images/taco.jpg" width="210px" height="210px">
-                <h2>Taco Bell</h2>
-                <p>4601 E. Main   Farmington, N.M.</p>
-            </div>
+                <div class="col-md-4 text-center">
+                    <img src="lib/images/blakes.png" width="210px" height="210px">
+                    <h2>Blakes Lotaburger</h2>
+                    <p>2130 Bloomfield Blvd.   Bloomfield,  N.M.</p>
+                </div>
 
-            <div class="col-md-4">
-                <img src="lib/images/taco.jpg" width="210px" height="210px">
-                <h2>Taco Bell</h2>
-                <p>2100 W. Main   Farmington, N.M.</p>
-            </div>
-        </div><!--row-->
+                <div class="col-md-4 text-center">
+                    <img src="lib/images/burgerking.png" width="210px" height="210px">
+                    <h2>Burger KIng</h2>
+                    <p>948 E. Main   Farmington,  N.M.</p>
+                </div>
+            </div><!--row-->
+
     </div><!--container-->
 
     <br><br><br>
 
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-4 text-center">
+                <img src="lib/images/burgerking.png" width="210px" height="210px">
+                <h2>Buger King</h2>
+                <p> 4450 E. Main   Farmington, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/burgerking.png" width="210px" height="210px">
+                <h2>Burger King</h2>
+                <p>710 W. Broadway   Bloomfield,  N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/burgerking.png" width="210px" height="210px">
+                <h2>Burger KIng</h2>
+                <p>Highway 666   Shiprock, N.M.</p>
+            </div>
+        </div><!--row-->
+
+        </div><!--container-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Fast Food</h1>
+        </div>
+    </div>
+
+        <div class="container">
+
+            <div class="row">
+
+                    <div class="col-md-4 text-center">
+                        <img src="lib/images/taco.jpg" width="210px" height="210px">
+                        <h2>Taco Bell</h2>
+                        <p>2226 E. Main   Farmington, N.M.</p>
+                    </div>
+
+                    <div class="col-md-4 text-center">
+                        <img src="lib/images/taco.jpg" width="210px" height="210px">
+                        <h2>Taco Bell</h2>
+                        <p>4601 E. Main   Farmington, N.M.</p>
+                    </div>
+
+                    <div class="col-md-4 text-center">
+                        <img src="lib/images/taco.jpg" width="210px" height="210px">
+                        <h2>Taco Bell</h2>
+                        <p>2100 W. Main   Farmington, N.M.</p>
+                </div>
+
+        </div><!--row-->
+
+    <br><br><br>
+
+        <div class="row">
+            <div class="col-md-4 text-center">
                 <img src="lib/images/tacobox.jpg" width="210px" height="210px">
                 <h2>Taco Box</h2>
                 <p>777 W. Broadway   Farmimgton,N.M.</p>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-4 text-center">
                 <img src="lib/images/teriyakibeefbowl.jpg" width="210px" height="210px">
                 <h2>Teriyaki Chicken & Beef Bowl</h2>
                 <p>3501 E. Main Ste H-Middlefork Sq    Frmt</p>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-4 text-center">
                 <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
                 <h2>Teriyaki Chicken Combo</h2>
                 <p>1808 E. Main   Farmington,N.M.</p>
             </div>
 
         </div><!--row-->
-    </div><!--container-->
 
     <br><br><br>
 
-    <div class="container">
-
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-4 text-center">
             <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
             <h2>Teriyaki Chicken Combo</h2>
             <p>Hwy 64  Kirtland, N.M.</p>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-4 text-center">
             <img src="lib/images/teriyakichickencombo.jpg" width="270px" height="210px">
             <h2>Teriyaki Chicken Combo</h2>
             <p>400 W.Broadway   Bloomfield, N.M.</p>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-4 text-center">
             <img src="lib/images/wendylogo.png" width="210px" height="210px">
             <h2>Wendy's Old Fashion Hamburgers</h2>
             <p>1820 E. Main   Frmt</p>
         </div>
     </div><!--row-->
 
-    </div><!--container-->
-
-
-    <div class="container">
-
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-4 text-center">
                 <img src="lib/images/wendylogo.png" width="210px" height="210px">
                 <h2>Wendy's Old Fashion Hamburgers</h2>
                 <p>4903 A  E. Main   Frmt</p>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-4 text-center">
                 <img src="lib/images/wendylogo.png" width="210px" height="210px">
                 <h2>Wendy's Old Fashion Hamburgers</h2>
                 <p>2601 W. Main     Frmt</p>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-4 text-center">
                 <img src="lib/images/wendylogo.png" width="210px" height="210px">
                 <h2>Wendy's Old Fashion Hamburgers</h2>
                 <p>1305 Aztec Blvd   Aztec</p>
             </div>
-
         </div><!--row-->
 
     </div><!--container-->
