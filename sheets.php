ba99236793f113c31d77fda551fb4c38cf3a16b4
DarrellJChristensen <68senior19@gmail.com>
2016-05-19 13:42:51 -0600

    added h2 message

--- sheets.php
+++ sheets.php
@@ -58,9 +58,9 @@
                 </div>
 
                 <div class="col-md-4 text-center">
-                    <img src="lib/images/burgerking.jpg" width="210px" height="210px">
-                    <h2>Burger KIng</h2>
-                    <p>948 E. Main   Farmington,  N.M.</p>
+                    <img src="lib/images/blakes.png" width="210px" height="210px">
+                    <h2>Blakes Lotaburger</h2>
+                    <p>420 S. Bloomfield Blvd.   Bloomfild,  N.M.</p>
                 </div>
             </div><!--row-->
 
@@ -68,10 +68,28 @@
 
         <div class="row">
             <div class="col-md-4 text-center">
+                <img src="lib/images/blakes.png" width="210px" height="210px">
+                <h2>Blakes Lotaburger</h2>
+                <p>210 W. Aztec Blvd.   Aztec, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/burgerking.jpg" width="210px" height="210px">
+                <h2>Burger KIng</h2>
+                <p>948 E. Main   Farmington,  N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
                 <img src="lib/images/burgerking.jpg" width="210px" height="210px">
                 <h2>Buger King</h2>
                 <p> 4450 E. Main   Farmington, N.M.</p>
             </div>
+
+        </div><!--row-->
+
+        <br><br><br>
+
+        <div class="row">
 
             <div class="col-md-4 text-center">
                 <img src="lib/images/burgerking.jpg" width="210px" height="210px">
@@ -84,31 +102,106 @@
                 <h2>Burger KIng</h2>
                 <p>Highway 666   Shiprock, N.M.</p>
             </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/churchs.png" width="210px" height="210px">
+                <h2>Churchs Chicken</h2>
+                <p>2711 E. 20th   Farmington, N.M.</p>
+            </div>
+
         </div><!--row-->
 
     <br><br><br>
 
-    <div class="row">
-        <div class="col-md-4 text-center">
-            <img src="lib/images/churchs.png" width="210px" height="210px">
-            <h2>Churchs Chicken</h2>
-            <p>2711 E. 20th   Farmington, N.M.</p>
-        </div>
+        <div class="row">
 
-        <div class="col-md-4 text-center">
-            <img src="lib/images/churchs.png" width="210px" height="210px">
-            <h2>Churchs Chicken</h2>
-            <p>5455 E. Main, N.M.</p>
-        </div>
+            <div class="col-md-4 text-center">
+                <img src="lib/images/churchs.png" width="210px" height="210px">
+                <h2>Churchs Chicken</h2>
+                <p>5455 E. Main, N.M.</p>
+            </div>
 
-        <div class="col-md-4 text-center">
-            <img src="lib/images/churchs.png" width="210px" height="210px">
-            <h2>Churchs Chicken</h2>
-            <p>725 W. Main   Farmington , N.M.</p>
-        </div>
-    </div><!--row-->
+            <div class="col-md-4 text-center">
+                <img src="lib/images/churchs.png" width="210px" height="210px">
+                <h2>Churchs Chicken</h2>
+                <p>725 W. Main   Farmington , N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/corndogsplus.jpg" width="210px" height="210px">
+                <h2>Corn Dogs Plus</h2>
+                <p>3000 E. 20th   Farmington , N.M.</p>
+            </div>
+
+        </div><!--row-->
 
     <br><br><br>
+
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <img src="lib/images/corndogsplusmall.jpg" width="210px" height="210px">
+                <h2>Corn Dogs Plus </h2>
+                <p>Animas Valley Mall   Farmington, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/dq.jpg" width="210px" height="210px">
+                <h2>Dairy Queen</h2>
+                <p>721 E. Main   Farmington, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/dq2.jpg" width="210px" height="210px">
+                <h2>Dairy Queen</h2>
+                <p>1908 N. Dustin Ave   Farmington, N.M.</p>
+            </div>
+        </div><!--row-->
+
+    <br><br><br>
+
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <img src="lib/images/dq.jpg" width="210px" height="210px">
+                <h2>Dairy Queen</h2>
+                <p>506 N.E.   Aztec, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/dominos2.png" width="210px" height="210px">
+                <h2>Domino's Pizza</h2>
+                <p>3000 E. 20th   Farmington, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/dominos3.png" width="210px" height="210px">
+                <h2>Domino's Pizza</h2>
+                <p>725 W. Main   Farmington, N.M.</p>
+            </div>
+        </div><!--row-->
+
+        <br><br><br>
+
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <img src="lib/images/dominos4.png" width="210px" height="210px">
+                <h2>Domino's Pizza</h2>
+                <p>1415 W. Aztec Blvd.   Aztec, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/dominos5.png" width="210px" height="210px">
+                <h2>Domino's Pizza</h2>
+                <p>204 W. Broadway   Bloomfield, N.M.</p>
+            </div>
+
+            <div class="col-md-4 text-center">
+                <img src="lib/images/dominos6.png" width="210px" height="210px">
+                <h2>Domino's Pizza</h2>
+                <p>4208 Hwy. 64   Kirtland, N.M.</p>
+            </div>
+        </div><!--row-->
+
+        <br><br><br>
 
 
 
