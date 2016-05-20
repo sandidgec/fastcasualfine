2e3397e0ba1704797c4a45213ac9733aeb0218ca
Chase Walters <riovista2797@gmail.com>
2016-05-20 12:39:51 -0600

    business and review classes done

--- lib/php/buisness.php
+++ lib/php/buisness.php
@@ -2,7 +2,7 @@
 <?php
 
 class Business implements JsonSerializable{
-    private $buisnessId;
+    private $businessId;
 
     private $email;
 
@@ -12,7 +12,31 @@ class Business implements JsonSerializable{
 
     private $website;
 
+    private $address;
 
+    private $zip;
+
+    public function __construct($newBusinessId, $newEmail, $newName, $newPhone, $newWebsite, $newAddress, $newZip )
+    {
+        try {
+            $this->setBusinessId($newBusinessId);
+            $this->setEmail($newEmail);
+            $this->setName($newName);
+            $this->setPhone($newPhone);
+            $this->setWebsite($newWebsite);
+            $this->setAddress($newAddress);
+            $this->setZip($newZip);
+        } catch (InvalidArgumentException $invalidArgument) {
+            //rethrow the exception to the caller
+            throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+        } catch (RangeException $range) {
+            // rethrow the exception to the caller
+            throw (new RangeException($range->getMessage(), 0, $range));
+        } catch (Exception $exception) {
+            // rethrow generic exception
+            throw(new Exception($exception->getMessage(), 0, $exception));
+        }
+    }
 
     /**
      * accessor method for businessId
@@ -29,20 +53,20 @@ class Business implements JsonSerializable{
      * @param int $newBuisnessId unique value to represent a user $newBuisnessId
      * @throws InvalidArgumentException for invalid content
      **/
-    public function setBusinessId($businessId)
+    public function setBusinessId($newBusinessId)
     {
 // base case: if the buisnessId is null,
 // this is a new user without a mySQL assigned id (yet)
-        if ($businessId === null) {
-            $this->businessId = null;
+        if ($newBusinessId === null) {
+            $this->newBusinessId = null;
             return;
         }
 //verify the User is valid
-        $businessId = filter_var($businessId, FILTER_VALIDATE_INT);
-        if (empty($businessId) === true) {
+        $newBusinessId = filter_var($newBusinessId, FILTER_VALIDATE_INT);
+        if (empty($newBusinessId) === true) {
             throw (new InvalidArgumentException ("buisnessID invalid"));
         }
-        $this->businessId = $businessId;
+        $this->businessId = $newBusinessId;
     }
 
     /**
@@ -88,9 +112,9 @@ class Business implements JsonSerializable{
     /**
      * Mutator method for First Name
      *
-     * @param string for user first name $newFirstName
+     * @param string $newName  for user first name $newFirstName
      */
-    public function setname($newName)
+    public function setName($newName)
     {
         // verify first name is valid
         $newName = filter_var($newName, FILTER_SANITIZE_STRING);
@@ -134,7 +158,7 @@ class Business implements JsonSerializable{
 
     public function getWebsite($newWebsite)
     {
-
+        return ($this->website);
     }
 
     /**
@@ -147,13 +171,13 @@ class Business implements JsonSerializable{
 
         $newWebsite = filter_var($newWebsite, FILTER_SANITIZE_STRING);
         if (strlen($newWebsite) > 128) {
-            throw (new RangeExecption ("Website is too long."));
+            throw (new RangeException ("Website is too long."));
         }
     }
 
 
-    public function getAddress($newAddress)
-    {
+    public function getAddress($newAddress){
+    return ($this->address);
     }
 
 
@@ -169,7 +193,7 @@ class Business implements JsonSerializable{
 
 
     public function getZip($newZip){
-
+        return ($this->zip);
     }
 
     public function setZip($newZip)
@@ -181,11 +205,113 @@ class Business implements JsonSerializable{
     }
 
 
+
    /* public function getSpeed($newSpeed){
             //!!!! !NEED HELP !!!!!
-    }
+    }      I do not kno how I would write this.
 
     Public function setSpeed($newSpeed){
 
     }*/
+    public function JsonSerialize()
+    {
+        $fields = get_object_vars($this);
+        return ($fields);
+    }
 }
+/**
+ * Inserts Bulletin into mySQL
+ *
+ * Inserts this bulletinId into mySQL in intervals
+ * @param PDO $pdo connection to
+ **/
+/**
+ * Inserts User into mySQL
+ *
+ * Inserts this userId into mySQL in intervals
+ * @param PDO $pdo connection to
+ **/
+public function insert(PDO &$pdo)
+{
+    // make sure user doesn't already exist
+    if ($this->buisnessId !== null) {
+        throw (new PDOException("existing user"));
+    }
+    //create query template
+    $query
+        = "INSERT INTO business(businessId, email, name, address, website, phone)
+        VALUES (businessId, email, name, address, website, phone)";
+    $statement = $pdo->prepare($query);
+    // bind the variables to the place holders in the template
+}
+public function delete(PDO &$pdo) {
+    // enforce the bulletin is not null
+    if($this->businessId === null) {
+        throw(new PDOException("unable to delete a bulletin that does not exist"));
+    }
+    //create query template
+    $query = "DELETE FROM business WHERE businessId = :businessId";
+    $statement = $pdo->prepare($query);
+    //bind the member variables to the place holder in the template
+    $parameters = array("businessId" => $this->businessId);
+    $statement->execute($parameters);
+
+/**
+ * Get all Bulletins
+ *
+ * @param PDO $pdo pointer to PDO connection, by reference
+ * @return mixed| buisness
+ **/
+public static function getAllBusiness(PDO &$pdo) {
+    // create query template
+    $query = "SELECT business, businessId, email, name, address, website, phone)";
+    $statement = $pdo->prepare($query);
+    // grab the bulletin from mySQL
+    try {
+        $bulletin = null;
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $row = $statement->fetch();
+        if($row !== false) {
+            $bulletin = new Business ($row["businessId"], $row["userId"], $row["email"], $row["name"], $row["address"], $row["website"], $row["phone"] );
+        }
+    } catch(Exception $exception) {
+        // if the row couldn't be converted, rethrow it
+        throw(new PDOException($exception->getMessage(), 0, $exception));
+    }
+    return ($bulletin);
+}
+/**
+ * get bulletin by category
+ *
+ * @param PDO $pdo pointer to PDO connection, by reference
+ * @param mixed info for $business
+ * @return null|buisness
+ **/
+public static function getBusinessByBusinessId(PDO &$pdo, $bulletin) {
+    if($bulletin === false) {
+        throw(new PDOException(""));
+    }
+    // create query template
+    $query = "SELECT businessId, name, phone, email, website, address, zip
+        FROM business WHERE businessId = :buisnessId";
+    $statement = $pdo->prepare($query);
+    // bind the bulletinid to the place holder in the template
+    $parameters = array("BusinessId" => $business);
+    $statement->execute($parameters);
+    // grab the bulletin from mySQL
+    try {
+        $review= null;
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $row = $statement->fetch();
+        if($row !== false) {
+            $buinsess = new Business ($row["businessId"], $row["userId"], $row["email"], $row["name"], $row["address"], $row["website"], $row["phone"] );
+        }
+    } catch(Exception $exception) {
+        // if the row couldn't be converted, rethrow it
+        throw(new PDOException($exception->getMessage(), 0, $exception));
+    }
+    return ($business);
+}
+
+
+
--- lib/php/review.php
+++ lib/php/review.php
@@ -1,29 +1,59 @@
 <?php
 
-class Review implements JsonSerializable
+class review implements JsonSerializable
 {
+    private $reviewID;
 
-    
-            const __default = self::Fast;
+    private $businessId;
 
-            const Fine = 1;
-            const Casual = 2;
-            const Fast = 3;
+    private $userId;
+
+    private $time;
+
+
+    public function __construct($newBusinessId, $newEmail, $newName, $newPhone, $newWebsite, $newAddress, $newZip )
+    {
+        try {
+            $this->setReviewID ($newReviewId);
+            $this->setBusinessId ($newBusinessId);
+            $this->setUserId ($newUserId);
+            $this->setTime($newTime);
+        } catch (InvalidArgumentException $invalidArgument) {
+            //rethrow the exception to the caller
+            throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+        } catch (RangeException $range) {
+            // rethrow the exception to the caller
+            throw (new RangeException($range->getMessage(), 0, $range));
+        } catch (Exception $exception) {
+            // rethrow generic exception
+            throw(new Exception($exception->getMessage(), 0, $exception));
+        }
+    }
+
+
+
+
+
+    const __default = self::Fast;
+
+    const Fine = 1;
+    const Casual = 2;
+    const Fast = 3;
 
 
     public function getReviewID($newReviewId)
     {
-        return ($this->reviewID)
+        return ($this->reviewID);
     }
 
 
     public function setReviewID($newReviewId)
     {
         if ($newReviewId === null) {
-            $this->reviewId = null;
+            $this->review = null;
             return;
         }
-        //verify the User is valid
+        //verify the review is valid
         $newReviewId = filter_var($newReviewId, FILTER_VALIDATE_INT);
         if (empty($newReviewId) === true) {
             throw (new InvalidArgumentException ("review ID is invalid"));
@@ -31,9 +61,9 @@ class Review implements JsonSerializable
         $this->userId = $newReviewId;
     }
 
-    public function getBuisnessId($newBuisnessId)
+    public function getBuisnessId($newBusinessId)
     {
-        return ($this->buisnessId);
+        return ($this->businessId);
     }
 
     /**
@@ -41,32 +71,169 @@ class Review implements JsonSerializable
      * @param int $newBuisnessId unique value to represent a user $newBuisnessId
      * @throws InvalidArgumentException for invalid content
      **/
-    public function setBuisnessId($newBuisnessId)
+    public function setBuisnessId($newBusinessId)
     {
 // base case: if the buisnessId is null,
 // this is a new user without a mySQL assigned id (yet)
-        if ($newBuisnessId === null) {
-            $this->buisnessId = null;
+        if ($newBusinessId === null) {
+            $this->businessId = null;
             return;
         }
 //verify the User is valid
-        $newBuisnessId = filter_var($newBuisnessId, FILTER_VALIDATE_INT);
-        if (empty($newBuisnessId) === true) {
+        $newBusinessId = filter_var($newBusinessId, FILTER_VALIDATE_INT);
+        if (empty($newBusinessId) === true) {
             throw (new InvalidArgumentException ("buisnessID invalid"));
         }
-        $this->buisnessId = $newBuisnessId;
-    }
-    
-//user id goes here
-
-    public function getTime($time){
-
+        $this->businessId = $newBusinessId;
     }
 
-    public function setTime($time){
-        if ($time) === null {
-            $this->time = null;
+    public function setUserId($newUserId)
+    {
+
+
+        if ($newUserId === null) {
+            $this->userId = null;
             return;
         }
+
+        $newUserId = filter_var($newUserId, FILTER_VALIDATE_INT);
+        if (empty($newUserId) === true) {
+            throw (new InvalidArgumentException ("userId invalid"));
+        }
+        $this->userId = $newUserId;
     }
+
+
+    public function getTime($time)
+    {
+
+    }
+
+    public function setTime($time)
+    {
+        if ($time === null) {
+        $this->time = null;
+        return;
+    }
+    }
+
+    function validateDate($time)
+    {
+        // base case: if the date is a DateTime object, there's no work to be done
+        if (is_object($time) === true && get_class($time) === "DateTime") {
+            return ($time);
+        }
+        // treat the date as a mySQL date string: Y-m-d H:i:s
+        $time = trim($time);
+        if ((preg_match("/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/", $time, $matches)) !== 1) {
+            throw(new InvalidArgumentException("date is not a valid date"));
+        }
+        // verify the date is really a valid calendar date
+        $year = intval($matches[1]);
+        $month = intval($matches[2]);
+        $day = intval($matches[3]);
+        $hour = intval($matches[4]);
+        $minute = intval($matches[5]);
+        $second = intval($matches[6]);
+        if (checkdate($month, $day, $year) === false) {
+            throw(new RangeException("date $time is not a Gregorian date"));
+        }
+
+
+
+    }
+    public function JsonSerialize()
+    {
+        $fields = get_object_vars($this);
+        return ($fields);
+    }
+}
+
+/**
+ * Inserts Bulletin into mySQL
+ *
+ * Inserts this bulletinId into mySQL in intervals
+ * @param PDO $pdo connection to
+ **/
+public function insert(PDO &$pdo)
+{
+    // make sure bulletin doesn't already exist
+    if ($this->ReviewId !== null) {
+        throw (new PDOException("existing bulletin"));
+    }
+    //create query template
+    $query
+        = "INSERT INTO review (userId, buisnessId, time)" .
+        "VALUES (:userId, :buisnessId, :time)";
+    $statement = $pdo->prepare($query);
+    // bind the variables to the place holders in the template
+    $parameters = array("userId" => $this->userId, "buisnessId" => $this->buisnessId, "time" => $this->time);
+    $statement->execute($parameters);
+    //update null bulletinId with what mySQL just gave us
+    $this->reviewId = intval($pdo->lastInsertId());
+
+    /**
+     * Deletes Bulletin from mySQL
+     *
+     * Delete PDO to delete bulletinId
+     * @param PDO $pdo
+     **/
+    public function delete(PDO &$pdo) {
+    // enforce the bulletin is not null
+    if($this->reviewId === null) {
+        throw(new PDOException("unable to delete a bulletin that does not exist"));
+    }
+    //create query template
+    $query = "DELETE FROM review WHERE ReviewId = :ReviewId";
+    $statement = $pdo->prepare($query);
+    //bind the member variables to the place holder in the template
+    $parameters = array("reviewId" => $this->ReviewId);
+    $statement->execute($parameters);
+}
+
+
+
+    public static function getAllReviews(PDO &$pdo) {
+    // create query template
+    $query = "SELECT reviewId, businessId, userId, time, FROM review";
+    $statement = $pdo->prepare($query);
+    // grab the bulletin from mySQL
+    try {
+        $review = null;
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $row = $statement->fetch();
+        if($row !== false) {
+            $review = new Review ($row["reviewId"], $row["businessId"], $row["userId"], $row["time"]);
+        }
+    } catch(Exception $exception) {
+        // if the row couldn't be converted, rethrow it
+        throw(new PDOException($exception->getReview(), 0, $exception));
+    }
+    return ($review);
+}
+    public static function getBusinessByBusinessId(PDO &$pdo, $bulletin) {
+    if($bulletin === false) {
+        throw(new PDOException(""));
+    }
+    // create query template
+    $query = "SELECT ReviewId, businessId, userId, time;
+        FROM review WHERE reviewId = :reviewId";
+    $statement = $pdo->prepare($query);
+    // bind the bulletinid to the place holder in the template
+    $parameters = array("reviewId" => $review);
+    $statement->execute($parameters);
+    // grab the bulletin from mySQL
+    try {
+        $review= null;
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $row = $statement->fetch();
+        if($row !== false) {
+            $review = new reviewId ($row["reviewId"], $row["businessId"], $row["userId"], $row["time"]);
+        }
+    } catch(Exception $exception) {
+        // if the row couldn't be converted, rethrow it
+        throw(new PDOException($exception->getMessage(), 0, $exception));
+    }
+    return ($business);
+}
 }
