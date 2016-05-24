1adb41b97a089f7567bdd55113426a7b439746ac
Jeremiah Johnson <jeremiahsir@gmail.com>
2016-05-24 10:43:14 -0600

    added more php code

--- lib/php/FCF.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config = array();
-$config ["hostname"] = "104.131.40.94";
-$config ["username"] = "fastcf";
-$config ["database"] = "fastcfdba";
-$config ["password"] = "fastcasualfine2016";
