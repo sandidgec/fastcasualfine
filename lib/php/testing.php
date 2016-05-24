8e2e6c9dd6cc8e465166be448a6684ae0112f198
Jeremiah Johnson <jeremiahsir@gmail.com>
2016-05-24 10:38:54 -0600

    added more php code

--- lib/php/testing.php
+++ lib/php/testing.php
@@ -7,7 +7,7 @@ $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 
 $pdo = new PDO($dsn, $config["username"], $config["password"], $options);
 
-$miss = new user();
+$miss = new user(null, );
 
 var_dump($miss);
 try{
