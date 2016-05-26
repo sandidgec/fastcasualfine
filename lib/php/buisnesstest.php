d6097a8d1fcef6cddb424d735f4ce8b888c338eb
Chase Walters <riovista2797@gmail.com>
2016-05-26 10:47:25 -0600

    business/index updated

---
+++ lib/php/buisnesstest.php
@@ -0,0 +1,18 @@
+<?php
+require_once ("../../fastcasualfine.php");
+require_once("autoload.php");
+
+
+
+
+
+// setup dsn and options
+$dsn = 'mysql:host=' . $config["hostname"] . ';dbname=' . $config["database"];
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+
+
+// set up pdo connection with database and set error attributes
+$pdo = new PDO($dsn, $config["username"], $config["password"], $options);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo "connected to DB";
