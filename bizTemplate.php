2b23f510b33c47c1e4fb033723f068dae5f88b5c
Justinmatece <justin_hildreth16@outlook.com>
2016-05-31 11:14:21 -0600

    added p tag

--- bizTemplate.php
+++ bizTemplate.php
@@ -5,101 +5,97 @@
     <?php require_once("lib/head-utils.php"); ?>
 </head>
 <h1> </h1>
-<body>
+<body class="background">
+
 <div class="jumbotron biz">
     <div class="container">
         <img class="picture" src="img/sisenor_logomod.jpg">
     </div>
 </div>
 <div class="container">
-    <div class="">...</div>   
+    <img src="lib/images/line.png" class="line">
+    <div class="row">
+        <div class="col-md-7">
+            <h2># About Us</h2>
+            <a href="http://sisenorfarmington.com/wp-content/uploads/2015/06/to-go-menu.pdf" target="blank"><img class="Menu" src="http://sisenorfarmington.com/wp-content/uploads/2012/05/hometile.jpg"></a>
+            <p>We have been serving everybodys favorite mexican food in the four corners since 1997!
+                We are a local Mexican Restaurant, that has been everyboys favoirte place to satisfy your hunger 100% </p>
+        </div>
+        <div class="col-md-5">
+            <h2># Contact Infoamation</h2>
+            <ul class="contact-list">
+                <li><img src="lib/images/phone.png"> (505) 324-9050</li><p>Call us today to resserve a reservation.</p>
+                <li><img src="lib/images/email.png"> Sisenor@gmail.com</li>
+                <li><img src="lib/images/facebook.png"> Facebook<span>facebook.com/SiSenor</span></li>
+                <li><img src="lib/images/twitter.png"> Twitter <span>twitter.com/SiSenor</span></li>
+                <li><img src="lib/images/Location-logo.png"> 4015 E 30th St, Farmington, NM 87402</li>
+            </ul>
+        </div>
+    </div>
 
-
-
-        </header>
-</div>
-
-        <div class="row">
-            <div class="col-md-7">
-                <h2># About Us</h2>
-                <p>We have been serving everybodys favorite mexican food in the four corners since 1997!
-                 We are a local Mexican Restaurant, that has been everyboys favoirte place to satisfy your hunger 100% </p>
+    <div class="row">
+        <div class="col-md-7">
+            <h2># Comments & Reviews</h2>
+            <div class="media">
+                <div class="media-left">
+                    1997 - Stil in bussiness
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">LoyalCustomer97</h4>
+                    <p>Every time i get off work, an to tired to cook for my family, i rely on Si Senor to satisfy
+                        our cravings, i love eating here, brings my family close together! </p>
+                </div>
             </div>
-            <div class="col-md-5">
-                <h2># Contact Infoamation</h2>
-                <ul class="contact-list">
-                    <li><img src="lib/images/phone.png"> (505) 324-9050</li>
-                    <li><img src="lib/images/email.png"> Sisenor@gmail.com</li>
-                    <li><img src="lib/images/facebook.png"> Facebook<span>facebook.com/SiSenor</span></li>
-                    <li><img src="lib/images/twitter.png"> Twitter <span>twitter.com/SiSenor</span></li>
-                </ul>
+
+            <div class="media">
+                <div class="media-left">
+                    2000 - 2003
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Joann Stine </h4>
+                    <p>Never a dull moment eating here. I absolutely LOVE IT!</p>
+                </div>
+            </div>
+
+            <div class="media">
+                <div class="media-left">
+                    2004 - 2008
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Lyle17</h4>
+                    <p>Eating here will always make me full, always stacking there plate of food to the max!</p>
+                </div>
+            </div>
+
+            <div class="media">
+                <div class="media-left">
+                    2009 - 2014
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Shane sky</h4>
+                    <p>Love this place so much i want to eat here for the rest of my life!</p>
+                </div>
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-md-7">
-                <h2># Comments & Reviews</h2>
-                <div class="media">
-                    <div class="media-left">
-                        1997 - Stil in bussiness
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">LoyalCustomer97</h4>
-                        <p>Every time i get off work, an to tired to cook for my family, i rely on Si Senor to satisfy
-                        our cravings, i love eating here, brings my family close together! </p>
-                    </div>
-                </div>
 
-                <div class="media">
-                    <div class="media-left">
-                        2000 - 2003
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">Joann Stine </h4>
-                        <p>Never a dull moment eating here. I absolutely LOVE IT!</p>
-                    </div>
-                </div>
-
-                <div class="media">
-                    <div class="media-left">
-                        2004 - 2008
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">Lyle17</h4>
-                        <p>Eating here will always make me full, always stacking there plate of food to the max!</p>
-                    </div>
-                </div>
-
-                <div class="media">
-                    <div class="media-left">
-                        2009 - 2014
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">Shane sky</h4>
-                        <p>Love this place so much i want to eat here for the rest of my life!</p>
-                    </div>
-                </div>
+        <div id="bottom_in">
+            <div class="restaurant_hours">
+                <h3>
+                    <cufon class="cufon cufon-canvas" alt="Restaurant" style="width; 137px; height: 24px;">
+                        <canvas width="164" height="28" style="width: 164px; height: 28px; top: -6px; left: -3px;">
+                            <cufontext>Restaurant</cufontext>
+                        </canvas>
+                    </cufon>
+                    <cufon class="cufon cufon-canvas" alt="Hours" style="width: 71px; height: 24px;">
+                        <canvas width="92" height="28" style="width: 92px; height: 28px; top: -6px; left: -3px;">
+                            <cufontext>Hours</cufontext>
+                    </cufon>
+                </h3>
             </div>
-
-
-    <div id="bottom_in">
-        <div class="restaurant_hours">
-            <h3>
-                <cufon class="cufon cufon-canvas" alt="Restaurant" style="width; 137px; height: 24px;">
-                    <canvas width="164" height="28" style="width: 164px; height: 28px; top: -6px; left: -3px;">
-                        <cufontext>Restaurant</cufontext>
-                    </canvas>
-                </cufon>
-                <cufon class="cufon cufon-canvas" alt="Hours" style="width: 71px; height: 24px;">
-                    <canvas width="92" height="28" style="width: 92px; height: 28px; top: -6px; left: -3px;">
-                        <cufontext>Hours</cufontext>
-                        </cufon>
-                         </h3>
-            
-
-
-
+        </div>
     </div>
+</div>
 
 
 
@@ -107,7 +103,8 @@
 
 
 
-<h1>Hello Dev Shop Team!</h1>
+
+
 
 </body>
 
