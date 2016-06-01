538bf5a56a0e49f5b79fe75c2fe5984352d335e6
Justinmatece <justin_hildreth16@outlook.com>
2016-06-01 14:04:51 -0600

    added p tag

---
+++ About.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once("lib/css/styles.css.php"); ?>
+
+    <title>about</title>
+</head>
+<body class="about-body">
+</body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Home</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="about.php">About</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav><br><br><br>
+<div>
+    <h> 4 Corners Food.com is a indispenable tool for the locals or an out of town visitor</h>
+    <h> in the Farmington area in search of a place to dine and want a rating system of the</h>
+    <h>restaurants along with location and contact information. It's an immensely valuable</h>
+    <h>marketing and advertising medium for the restaurant owner.</h>
+    
+</div><br><br>
+
+<div class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+        <p class="navbar-text pull-center">Copyright 2016 Cultivating Coders</p>
+    </div>
+</div>
+
+</html>
