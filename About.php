575ca12c39575c8e64e2b5e6d82f502836cd0ef2
Justinmatece <justin_hildreth16@outlook.com>
2016-06-01 15:46:34 -0600

    added p tag

--- About.php
+++ About.php
@@ -1,43 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php require_once("lib/css/styles.css"); ?>
-
+    <link rel="stylesheet" href="lib/css/about.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <title>About</title>
 </head>
-<body class="about-body">
+<body>
 
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">Home</a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="About.php">About</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav><br><br><br>
-<div>
+<?php require_once("lib/navbar.php");?>
+<div class="jumbotron">
+    <div class="container">
+        <h1>About</h1>
+    </div>
+</div>
+<div class="container">
     <h1> 4 Corners Food.com is a indispenable tool for the locals or an out of town visitor</h1>
     <h1> in the Farmington area in search of a place to dine and want a rating system of the</h1>
     <h1>restaurants along with location and contact information. It's an immensely valuable</h1>
     <h1>marketing and advertising medium for the restaurant owner.</h1>
 
-</div><br><br>
-
+</div>
 <div class="navbar navbar-default navbar-fixed-bottom">
     <div class="container">
         <p class="navbar-text pull-center">Copyright 2016 Cultivating Coders</p>
