e47858835e63d98e024246272c6cf83b85dfb74f
Chase Walters <riovista2797@gmail.com>
2016-06-01 12:55:29 -0600

    Admin pages are coming

---
+++ businessAdmin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BusinessAdmin</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <form>
+                <div class="form-group">
+                    <label for="Name">Name</label>
+                    <input type="text" class="form-control" id="name" name="Name">
+                </div>
+                <div class="form-group">
+                    <label for="Address">Address</label>
+                    <input type="text" class="form-control" id="address" name="Address">
+                </div>
+                <div>
+                    <label for="Eamil">Email</label>
+                    <input type="text" class="form-control" id="Eamil" name="Email">
+                </div>
+                <div class="form-group">
+                    <label for="Phone">Phone</label>
+                    <input type="number" class="form-control" id="Phone" name="Phone">
+                </div>
+                <div class="form-group">
+                    <label for="Speed">Website</label>
+                    <input type=""
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
