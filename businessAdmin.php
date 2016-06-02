7fe8f0fc0249bbd366ee67b431fa54b23a10b619
Chase Walters <riovista2797@gmail.com>
2016-06-02 11:32:20 -0600

    Admin Page almost done

--- businessAdmin.php
+++ businessAdmin.php
@@ -5,78 +5,99 @@
     <link rel="stylesheet" href="lib/css/adminStyle.css"/>
 </head>
 <body>
+<?php require_once("lib/navbar.php") ?>
+<div class="container">
+    <h2>Business Submition Page</h2>
+</div>
 <div class="container">
     <div class="row">
         <div class="col-md-6">
             <form>
                 <div class="form-group">
                     <label for="Name">Name</label>
+                    <div id="">
                     <input type="text" class="form-control" id="name" name="Name">
+                        </div>
                 </div>
                 <div class="form-group">
                     <label for="Address">Address</label>
+                    <div>
                     <input type="text" class="form-control" id="address" name="Address">
+                    </div>
                 </div>
-                <div>
-                    <label for="Eamil">Email</label>
+                <div class="form-group">
+                    <label for="Email">Email</label>
+                    <div>
                     <input type="text" class="form-control" id="email" name="Email">
+                    </div>
                 </div>
                 <div class="form-group">
                     <label for="Phone">Phone</label>
+                    <div>
                     <input type="number" class="form-control" id="phone" name="Phone">
+                    </div>
                 </div>
                 <div class="form-group">
                     <label for="Speed">Website</label>
+                    <div>
                     <input type="text" class="form-control" id="speed" name="Speed">
                 </div>
                 <div class="form-group">
                     <label for="Zip">Zip Code</label>
+                    <div>
                     <input type="number" class="form-control" id="zip" name="Zip">
+                    </div>
                 </div>
-                <!--<div class="form-group">
+                <div class="form-group">
                    <label for="Images">Images</label>
                    <input type="image" class="form-control" id="images" name="Images">
-               </div>-->
+               </div>
                 <div class="form-group">
                     <label for="Submit">Submit</label>
+                    <div>
                     <input type="submit" class="form-control" id="submit" name="Submit">
+                    </div>
                 </div>
             </form>
         </div>
     </div>
 </div>
-<div>
-    <table class="businessAdminTable">
+<div id="table">
+    <table class="businessAdminTable" align="center">
         <tr>
-            <td>INFO</td>
+            <td>Business Table</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>BusinessID</td>
             <td>FAKE INFO</td>
         </tr>
         <tr>
-            <td>INFO</td>
+            <td>Business Name</td>
             <td>Stuff here</td>
         </tr>
         <tr>
-            <td>TITLE</td>
+            <td>Address</td>
             <td>Stuff</td>
         </tr>
         <tr>
-            <td>TITLE</td>
+            <td>Email</td>
             <td>Stuff</td>
         </tr>
         <tr>
-            <td>TITEL</td>
+            <td>Phone</td>
             <td>Stuff</td>
         </tr>
         <tr>
-            <td>TITEL</td>
+            <td>Website</td>
             <td>Stuff</td>
         </tr>
         <tr>
-            <td>SEVEN</td>
+            <td>Images</td>
             <td>Stuff</td>
         </tr>
         <tr>
-            <td>Eight</td>
+            <td>Zip Code</td>
             <td>Stuff</td>
         </tr>
     </table>
