767c7743bfc9590f796f8b7455e62ee77b4dc73a
Chase Walters <riovista2797@gmail.com>
2016-06-02 17:19:56 -0600

    NavBar change

--- lib/navbar.php
+++ lib/navbar.php
@@ -9,16 +9,16 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">Home</a>
+            <a class="navbar-brand" href="index.php"><i class="fa fa-cutlery" aria-hidden="true"></i></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="dropdown">
-                    <a href="admin/businesses.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Business<span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Business<span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <!--<li><a href="">Business</a></li>-->
+                        <li><a href="admin/businesses.php">Business</a></li>
                         <!--  <li><a href="">groups</a></li>
                           <li><a href="">projects</a></li>
                      <li role="separator" class="divider"></li>
@@ -28,9 +28,9 @@
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="About.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About<span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <!--<li><a href="">About</a></li>-->
+                        <li><a href="About.php">About</a></li>
                         <!--<li><a href=""></a></li>-->
                         <!--<li><a href="">lyrics</a></li>
                         <li><a href="">ideas</a></li>
@@ -43,9 +43,9 @@
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="contact.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <!--<li><a href="">Contact</a></li>-->
+                        <li><a href="contact.php">Contact</a></li>
                     </ul>
                 </li>
                 <!--<form class="navbar-form pull-right" role="search">
