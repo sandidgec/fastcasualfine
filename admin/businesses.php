3409e1188164ef12ede0fb2268229932b8c501ca
Bjorkbat <bjorkbat@gmail.com>
2016-06-02 09:53:25 -0600

    frontend can now async send businesses

--- admin/businesses.php
+++ admin/businesses.php
@@ -9,9 +9,10 @@
       crossorigin="anonymous">
 
   </head>
-  <body>
+  <body ng-app="FCFBusiness">
 
-    <div class="modal fade" tabindex="-1" role="dialog" id="newBusinessModal">
+    <div class="modal fade" tabindex="-1" role="dialog" id="newBusinessModal"
+      ng-controller="BusinessFormCtrl">
       <div class="modal-dialog">
         <div class="modal-content">
 
@@ -22,41 +23,29 @@
               <span aria-hidden="true">&times;</span>
             </button>
 
-            <h4 class="modal-title">Add New Mission</h4>
+            <h4 class="modal-title">Add New Business</h4>
           </div>
 
-          <form>
+          <form ng-submit="sendBusiness()">
 
             <div class="modal-body">
 
               <div class="form-group">
                 <label for="#inputName">Name</label>
                 <input type="text" class="form-control" id="inputName"
-                  name="name">
+                  name="name" ng-model="businessName">
               </div>
 
               <div class="form-group">
                 <label for="#inputAddr">Address</label>
                 <input type="text" class="form-control" id="inputAddr"
-                  name="addr">
+                  name="addr" ng-model="address">
               </div>
 
               <div class="form-group">
-                <label for="#inputEmail">Email</label>
-                <input type="email" class="form-control" id="inputEmail"
-                  name="email">
-              </div>
-
-              <div class="form-group">
-                <label for="#inputPhone">Phone</label>
-                <input type="text" class="form-control" id="inputPhone"
-                  name="phone">
-              </div>
-
-              <div class="form-group">
-                <label for="#inputSpeed">Speed</label>
-                <input type="text" class="form-control" id="inputSpeed"
-                  name="speed">
+                <label for="#inputZip">Zip</label>
+                <input type="text" class="form-control" id="inputZip"
+                  name="zip" ng-model="zip">
               </div>
 
               <div class="form-group">
@@ -66,17 +55,22 @@
               </div>
 
               <div class="form-group">
-                <label for="#inputWebsite">Website</label>
-                <input type="text" class="form-control" id="inputWebsite"
-                  name="website">
+                <label for="#inputEmail">Email</label>
+                <input type="email" class="form-control" id="inputEmail"
+                  name="email" ng-model="email">
               </div>
 
               <div class="form-group">
-                <label for="#inputZip">Zip</label>
-                <input type="text" class="form-control" id="inputZip"
-                  name="zip">
+                <label for="#inputWebsite">Website</label>
+                <input type="text" class="form-control" id="inputWebsite"
+                  name="website" ng-model="website">
               </div>
 
+              <div class="form-group">
+                <label for="#inputSpeed">Speed</label>
+                <input type="text" class="form-control" id="inputSpeed"
+                  name="speed" ng-model="speed">
+              </div>
             </div>
 
             <div class="modal-footer">
@@ -107,6 +101,9 @@
       integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
       crossorigin="anonymous">
     </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.6/angular.min.js"></script>
+    <script src="/lib/js/admin-missions.js"></script>
 
   <body>
 </html>
